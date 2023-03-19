e9b2fc3d42d399dd5ccdb6787610e9e90f8464b7
lsaenzmarmol <lsaenzmarmol@gmail.com>
2023-03-19 05:25:58 -0400

    Better camera follow system and enemies now die when shot as well as bullets destroying after 3 sec

---
+++ Slime and Grime/Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float jumpSpeed;
+
+    private CharacterController characterController;
+    private float ySpeed;
+    private float originalStepOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = jumpSpeed;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+
+        Vector3 velocity = movementDirection * magnitude;
+
+        velocity.y = ySpeed;
+
+        characterController.Move(movementDirection * magnitude);
+        
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+    }
+}
