777c60a67b49a7a5c73ba965cd5f4ab4b92e2708
lsaenzmarmol <lsaenzmarmol@gmail.com>
2023-03-29 04:27:36 -0400

    attempt at better mechanics trying to set up gumi gumi

--- Slime and Grime/Assets/Scripts/PlayerMovement.cs
+++ Slime and Grime/Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{   
     public float speed = 2.0f;
+    
     private float horizontalInput;
     private float forwardInput;
     // Start is called before the first frame update
@@ -23,4 +24,5 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector3.forward* Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right* Time.deltaTime * speed * horizontalInput);
     }
+    
 }
