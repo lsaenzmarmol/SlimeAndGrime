fe4434b957c88c1ffcc76dc40393315bfd3405bb
Skyler Donovan <skydon13@knights.ucf.edu>
2023-03-31 19:28:49 -0400

    commit

---
+++ Slime and Grime/Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("isWalking", true);
+
+        }
+
+
+        if (Input.GetKeyUp("w"))
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+
+        if (Input.GetKey("s"))
+        {
+            anim.SetBool("isWalking", true);
+
+        }
+
+
+        if (Input.GetKeyUp("s"))
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+
+        if (Input.GetKey("d"))
+        {
+            anim.SetBool("isWalking", true);
+
+        }
+
+        if (Input.GetKeyUp("d"))
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+
+
+        if (Input.GetKey("a"))
+        {
+            anim.SetBool("isWalking", true);
+
+        }
+
+        if (Input.GetKeyUp("a"))
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isLanding", true);
+        }
+
+
+
+
+
+        if (Input.GetKey("space"))
+        {
+            anim.SetBool("isJumping", true);
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            anim.SetBool("isJumping", false);
+        }
+
+    }
+}
